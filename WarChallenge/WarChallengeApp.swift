6e0c66561ffe494c98ba6f6a93ed6569bcaa3074
Datafox-JCP <datafox@outlook.com>
2022-07-07 07:59:10 -0500

    Initial Commit

---
+++ WarChallenge/WarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarChallengeApp.swift
+//  WarChallenge
+//
+//  Created by Juan Hernandez Pazos on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WarChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
